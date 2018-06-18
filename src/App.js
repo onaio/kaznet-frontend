5b7af1699ad5c7a3b7513b8cd49f885eb6a52276
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-06-18 17:15:28 +0300

    Introduce bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { Route, Switch } from 'react-router';
 import { NavLink } from 'react-router-dom';
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  Container,
+  Row,
+  Col
+} from 'reactstrap';
+
+import profile_image from './images/profile.png'
+import './App.css';
 import Home from './components/Home';
 import NoMatch from './components/NoMatch';
 import TasksList from './containers/tasks/TasksList';
@@ -12,19 +25,48 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-          <nav>
-            <NavLink to="/" activeClassName="selected">Home</NavLink>
-            <NavLink to="/tasks" activeClassName="selected">Tasks</NavLink>
-          </nav>
+        <header>
+          <Container fluid>
+            <Navbar dark color="dark" expand="md">
+              <NavbarBrand href="/">[Kaznet Logo]</NavbarBrand>
+              <NavbarToggler />
+              <Collapse navbar>
+                <Nav navbar>
+                  <NavItem>
+                    <NavLink to="/tasks" className="nav-link" activeClassName="active">Tasks</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink to="#" className="nav-link" activeClassName="active">Clients</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink to="#" className="nav-link" activeClassName="active">Locations</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink to="#" className="nav-link" activeClassName="active">Users</NavLink>
+                  </NavItem>
+                </Nav>
+                <Nav className="ml-auto" navbar>
+                  <NavItem>
+                    <img src={profile_image} className="img-fluid rounded-circle userprofile-img" alt="profile image" />
+                  </NavItem>
+                </Nav>
+              </Collapse>
+            </Navbar>
+          </Container>
         </header>
-        <Switch>
-          <Route path="/tasks" component={TasksList}/>
-          <Route exact path="/" component={Home}/>
-          <Route component={NoMatch}/>
-        </Switch>
+        <main role="main">
+          <Container fluid>
+            <Row>
+              <Col>
+                <Switch>
+                  <Route exact path="/tasks" component={TasksList}/>
+                  <Route exact path="/" component={Home}/>
+                  <Route component={NoMatch}/>
+                </Switch>
+              </Col>
+            </Row>
+          </Container>
+        </main>
       </div>
     );
   }
