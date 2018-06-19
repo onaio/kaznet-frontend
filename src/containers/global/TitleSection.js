d16d9522ff4196070404a36513bb0ba0d9c04305
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-06-19 16:10:26 +0300

    Add TitleSection container

---
+++ src/containers/global/TitleSection.js
@@ -0,0 +1,28 @@
+// Smart component that renders the page title section
+import React, { Component } from 'react';
+import {connect} from "react-redux";
+
+import PageTitle from '../../components/page/PageTitle';
+import * as globalSelectors from '../../store/global/reducer';
+
+
+class TitleSection extends Component {
+    render() {
+        return(
+            <PageTitle
+                pageTitle={this.props.pageTitle}
+                pageTitleButton={this.props.pageTitleButton}
+            />
+        );
+    }
+}
+
+// which props do we want to inject, given the global store state?
+function mapStateToProps(state) {
+    return {
+        pageTitle : globalSelectors.getPageTitle(state),
+        pageTitleButton : globalSelectors.getPageTitleButton(state)
+    };
+}
+
+export default connect(mapStateToProps)(TitleSection);
