59cd5a17c613cf7e03d03ca19a3d2fb1c89a8125
DavisRayM <davisraymondmuro@outlook.com>
2018-06-19 13:04:31 +0300

    Modify ClientsList -Delete CLientListView

--- src/containers/clients/ClientsList.js
+++ src/containers/clients/ClientsList.js
@@ -2,19 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as clientActions from '../../store/clients/actions';
 import * as clientSelectiors from '../../store/clients/reducer';
-import ListView from '../../components/ClientListView'
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import './ClientsList.css'
-import {
-  Row,
-  Col,
-  Form,
-  Button,
-  Input,
-  InputGroup,
-  InputGroupAddon,
-  InputGroupText
-} from 'reactstrap';
+import ListView from '../../components/ListView'
+import ElementMap from '../ElementMap';
 
 class ClientsList extends Component {
 
@@ -25,39 +14,12 @@ class ClientsList extends Component {
     render() {
         if(!this.props.rowsById) return this.renderLoading();
         return (
-            <div className="Clients">
-                <section className="page-title">
-                    <Row>
-                        <Col sm="12" md={{ size: 8, offset: 2 }}>
-                        <div>
-                            <h1>Page Title</h1>
-                            <Row>
-                            <Col md="9">
-                                <Form>
-                                <InputGroup className="search-group">
-                                    <InputGroupAddon addonType="prepend" className="search-prepend">
-                                    <InputGroupText className="bg-white border-right-0">
-                                        <FontAwesomeIcon icon="search" />
-                                    </InputGroupText>
-                                    </InputGroupAddon>
-                                    <Input type="text" bsSize="lg" className="border-left-0" placeholder="Search" aria-label="Search" />
-                                </InputGroup>
-                                </Form>
-                            </Col>
-                            <Col md="3">
-                                <Button block size="lg" color="primary">+ Add Client</Button>
-                            </Col>
-                            </Row>
-                        </div>
-                        </Col>
-                    </Row>
-                </section>
-                <div className="ClientsList">
-                    <ListView
+            <div className="ClientsList">
+                <ListView
+                    renderHeaders={this.renderHeaders}
                     rowsIdArray={this.props.rowsIdArray}
                     rowsById={this.props.rowsById}
                     renderRow={this.renderRow} />
-                </div>
             </div>
         );
     }
@@ -68,11 +30,21 @@ class ClientsList extends Component {
         );
     }
 
-    renderRow(row) {
+    renderHeaders() {
+        const headerItems = [
+          'Name',
+        ];
         return (
-            [
-                <td>{row.attributes.name}</td>
-            ]
+          <ElementMap items={headerItems} HTMLTag='th' />
+        );
+      }
+
+    renderRow(row) {
+        const rowItems = [
+          row.attributes.name,
+        ];
+        return (
+          <ElementMap items={rowItems} HTMLTag='td' />
         );
     }
 }
