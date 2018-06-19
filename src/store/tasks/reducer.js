2ebaaa8e0468d77d888275ddaf1cbc8f85848881
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-06-19 11:10:44 +0300

    Code cleanup

--- src/store/tasks/reducer.js
+++ src/store/tasks/reducer.js
@@ -1,8 +1,3 @@
-// reducers hold the store's state (the initialState object defines it)
-// reducers also handle plain object actions and modify their state (immutably) accordingly
-// this is the only way to change the store's state
-// the other exports in this file are selectors, which is business logic that digests parts of the store's state
-// for easier consumption by views
 import _ from 'lodash';
 import Immutable from 'seamless-immutable';
 
