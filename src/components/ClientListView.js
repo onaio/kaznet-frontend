8d5d803b3c6922c45ea7b5924d1e4b2931ba4c33
DavisRayM <davisraymondmuro@outlook.com>
2018-06-19 11:01:44 +0300

    Rename ListView to TaskListView - Add ClientListView

---
+++ src/components/ClientListView.js
@@ -0,0 +1,30 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { Table } from 'reactstrap';
+
+export default class ListView extends Component {
+
+  render() {
+    return (
+      <Table bordered className="kaznet-table">
+        <thead>
+          <tr>
+            <th id='header'>Name</th>
+          </tr>
+        </thead>
+        <tbody id='client_name'>
+          {_.map(this.props.rowsIdArray, this.renderRowById.bind(this))}
+        </tbody>
+      </Table>
+    );
+  }
+
+  renderRowById(rowId) {
+    return (
+      <tr key={rowId}>
+        {this.props.renderRow(_.get(this.props.rowsById, rowId))}
+      </tr>
+    );
+  }
+
+}
