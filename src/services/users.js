3711073857ff876d1e62132649b23668cf623c69
DavisRayM <davisraymondmuro@outlook.com>
2018-06-19 12:08:25 +0300

    Add UserService

---
+++ src/services/users.js
@@ -0,0 +1,30 @@
+import _ from 'lodash';
+
+import * as constants from '../constants';
+
+class UserService {
+
+  async getUserList() {
+    const url = `${constants.KAZNET_ENDPOINT}/userprofiles/?format=vnd.api%2Bjson`;
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/vnd.api+json',
+      }
+    });
+    if (!response.ok) {
+      throw new Error(`UserService getUserList failed, HTTP status ${response.status}`);
+    }
+    const apiResponse = await response.json();
+    const data = _.get(apiResponse, 'data');
+    if (!data) {
+      throw new Error(`UserService getUserList failed, data not returned`);
+    }
+    return _.map(data, (user) => {
+      return user;
+    });
+  }
+
+}
+
+export default new UserService();
