1f1f924abca7201b4f6e23910d547263c9a67b0d
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-06-19 16:33:40 +0300

    Handle global actions

--- src/store/global/reducer.js
+++ src/store/global/reducer.js
@@ -1,13 +1,27 @@
 // Global reducer
 import Immutable from 'seamless-immutable';
 
+import * as types from './actionTypes';
+
 const initialState = Immutable({
     pageTitle: 'Kaznet',
     pageTitleButton: 'Friendly Button',
 });
 
 export default function reduce(state = initialState, action = {}) {
-    return state;
+    // return state;
+    switch (action.type) {
+        case types.CHANGE_PAGETITLE:
+          return state.merge({
+            pageTitle: action.pageTitle
+          });
+        case types.CHANGE_PAGETITLE_BUTTON:
+          return state.merge({
+            pageTitleButton: action.pageTitleButton
+          });
+        default:
+          return state;
+    }
 }
 
 // selectors
