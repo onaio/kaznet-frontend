a33993fdc3797ef6edea127538aaabefa58b710f
DavisRayM <davisraymondmuro@outlook.com>
2018-06-19 11:27:17 +0300

    Add locations reducer

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -2,9 +2,11 @@
 import global from './global/reducer';
 import tasks from './tasks/reducer';
 import users from './users/reducer'
+import locations from './locations/reducer'
 
 export {
   tasks,
   global,
-  users
+  users,
+  locations
 };
