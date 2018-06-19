918936be848b2a69e3987635e74d6c1af46240d6
DavisRayM <davisraymondmuro@outlook.com>
2018-06-19 11:03:03 +0300

    Add client reducer to reducers.js

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -3,10 +3,12 @@ import global from './global/reducer';
 import tasks from './tasks/reducer';
 import users from './users/reducer'
 import locations from './locations/reducer'
+import clients from './clients/reducer'
 
 export {
   tasks,
   global,
   users,
-  locations
+  locations,
+  clients
 };
