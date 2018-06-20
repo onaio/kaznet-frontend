3bd21a9008d220557f7b4405743edf1551af1b61
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-06-20 14:59:00 +0300

    ErrorBoundary test

---
+++ src/components/tests/ErrorBoundary.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { shallow } from "enzyme";
+import ErrorBoundary from "../ErrorBoundary";
+
+describe("components/ErrorBoundary", () => {
+  it("renders without crashing", () => {
+    shallow(<ErrorBoundary />);
+  });
+});
