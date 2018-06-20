918698eb0877a455c7d951e8b74b164cc745297e
DavisRayM <davisraymondmuro@outlook.com>
2018-06-20 14:10:46 +0300

    Update Users Service

--- src/services/users.js
+++ src/services/users.js
@@ -1,31 +1,31 @@
-import _ from 'lodash';
+import _ from "lodash";
 
-import * as constants from '../constants';
+import * as constants from "../constants";
 
 class UserService {
-
   async getUserList() {
     const url = `${constants.API_ENDPOINT}/userprofiles/?format=vnd.api%2Bjson`;
     const response = await fetch(url, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        Accept: 'application/vnd.api+json',
-        Authorization: `Token ${constants.API_TOKEN}`
+        Accept: "application/vnd.api+json",
+        Authorization: `Token b4dfde6232d8f696713a7fde59e8f05ecfded42d`
       }
     });
     if (!response.ok) {
-      throw new Error(`UserService getUserList failed, HTTP status ${response.status}`);
+      throw new Error(
+        `UserService getUserList failed, HTTP status ${response.status}`
+      );
     }
     const apiResponse = await response.json();
-    const data = _.get(apiResponse, 'data');
+    const data = _.get(apiResponse, "data");
     if (!data) {
       throw new Error(`UserService getUserList failed, data not returned`);
     }
-    return _.map(data, (user) => {
+    return _.map(data, user => {
       return user;
     });
   }
-
 }
 
 export default new UserService();
