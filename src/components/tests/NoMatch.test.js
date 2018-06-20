148cef35f02673b7440efac444c4de552b3c516a
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-06-20 14:59:23 +0300

    NoMatch test

---
+++ src/components/tests/NoMatch.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { shallow } from "enzyme";
+import NoMatch from "../NoMatch";
+
+describe("components/NoMatch", () => {
+  it("renders without crashing", () => {
+    shallow(<NoMatch />);
+  });
+
+  it("renders the 404 message", () => {
+    const wrapper = shallow(<NoMatch />);
+    const expected = "404!!";
+    expect(wrapper.contains(expected)).toEqual(true);
+  });
+});
