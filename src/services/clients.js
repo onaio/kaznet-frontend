da41dd2d0c170428dd1a2474fcfdb98038854214
DavisRayM <davisraymondmuro@outlook.com>
2018-06-20 14:24:14 +0300

    Update Clients Service

--- src/services/clients.js
+++ src/services/clients.js
@@ -1,32 +1,34 @@
-import _ from 'lodash';
-import * as constants from '../constants';
+import _ from "lodash";
+import * as constants from "../constants";
 
 class ClientService {
+  async getClientList() {
+    const url = `${constants.API_ENDPOINT}/clients/?format=vnd.api%2Bjson`;
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/vnd.api+json",
+        Authorization: `Token ${constants.API_TOKEN}`
+      }
+    });
 
-    async getClientList() {
-        const url = `${constants.KAZNET_ENDPOINT}/clients/?format=vnd.api%2Bjson`;
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                Accept: 'application/vnd.api+json'
-            }
-        });
-
-        if (!response.ok) {
-            throw new Error(`ClientService getClientList failed, HTTP status ${response.status}`);  
-        }
-
-        const apiResponse = await response.json();
-        const data = _.get(apiResponse, 'data');
-
-        if (!data) {
-            throw new Error(`ClientService getClientList failed, data not returned`);
-        }
-
-        return _.map(data, (client) => {
-            return client;
-        });
+    if (!response.ok) {
+      throw new Error(
+        `ClientService getClientList failed, HTTP status ${response.status}`
+      );
     }
+
+    const apiResponse = await response.json();
+    const data = _.get(apiResponse, "data");
+
+    if (!data) {
+      throw new Error(`ClientService getClientList failed, data not returned`);
+    }
+
+    return _.map(data, client => {
+      return client;
+    });
+  }
 }
 
 export default new ClientService();
