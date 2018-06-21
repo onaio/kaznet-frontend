e8b10a73369a7f317d628ad6a202464150c19a91
DavisRayM <davisraymondmuro@outlook.com>
2018-06-21 11:24:02 +0300

    Add Tests for UsersList Container

---
+++ src/containers/users/tests/UsersList.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+import toJson from "enzyme-to-json";
+
+import { UsersList } from "../UsersList";
+import * as fixtures from "../../../store/users/tests/fixtures";
+
+describe("containers/users/UsersList", () => {
+  it("renders without crashing", () => {
+    shallow(
+      <UsersList
+        fetchUsers={function() {}}
+        changePageTitle={function() {}}
+        changePageTitleButton={function() {}}
+      />
+    );
+  });
+
+  it("renders user list correctly", () => {
+    const wrapper = mount(
+      <UsersList
+        fetchUsers={function() {}}
+        changePageTitle={function() {}}
+        changePageTitleButton={function() {}}
+      />
+    );
+    expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper.unmount();
+  });
+});
