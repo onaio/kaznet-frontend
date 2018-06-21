5a05551354cfccea06fc26e0c285c47836e4dc07
DavisRayM <davisraymondmuro@outlook.com>
2018-06-21 12:36:30 +0300

    Add Intergration tests for global and tasks store

---
+++ src/store/tests/tasks.test.js
@@ -0,0 +1,32 @@
+// task store Integration tests
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import thunk from "redux-thunk";
+
+import * as reducers from "../reducers";
+import * as taskActions from "../tasks/actions";
+import * as taskSelectors from "../tasks/reducer";
+import * as fixtures from "../tasks/tests/fixtures";
+import TaskService from "../../services/tasks";
+
+jest.mock("../../services/tasks");
+
+describe("store/topics integration", () => {
+  let store;
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+    store = createStore(combineReducers(reducers), applyMiddleware(thunk));
+  });
+
+  it("should retrieve all tasks", async () => {
+    TaskService.getTaskList.mockReturnValueOnce(fixtures.tasksArray);
+
+    await store.dispatch(taskActions.fetchTasks());
+    expect(taskSelectors.getTasksById(store.getState())).toEqual(
+      fixtures.tasksById
+    );
+    expect(taskSelectors.getTasksIdArray(store.getState())).toEqual(
+      fixtures.tasksIdArray
+    );
+  });
+});
