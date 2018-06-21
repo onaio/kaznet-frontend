45e79776189f1210d1361043373426d0b0069645
DavisRayM <davisraymondmuro@outlook.com>
2018-06-21 13:17:01 +0300

    Update Locations reducer

--- src/store/locations/reducer.js
+++ src/store/locations/reducer.js
@@ -1,28 +1,29 @@
-import _ from 'lodash';
-import Immutable from 'seamless-immutable';
+// Locations reducer
+import _ from "lodash";
+import Immutable from "seamless-immutable";
 
-import * as types from './actionTypes';
+import * as types from "./actionTypes";
 
 const initialState = Immutable({
-    "locationsById": {},
-    "locationsIdArray": []
+  locationsById: {},
+  locationsIdArray: []
 });
 
 export default function reduce(state = initialState, action = {}) {
-    switch (action.type) {
-        case types.LOCATIONS_FETCHED:
-            return state.merge({
-                locationsById: action.locationsById
-            });
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case types.LOCATIONS_FETCHED:
+      return state.merge({
+        locationsById: action.locationsById
+      });
+    default:
+      return state;
+  }
 }
 
 export function getLocationsById(state) {
-    return state.locations.locationsById;
+  return state.locations.locationsById;
 }
 
 export function getLocationsIdArray(state) {
-    return _.keys(state.locations.locationsById);
+  return _.keys(state.locations.locationsById);
 }
