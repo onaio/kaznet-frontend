8d0f5a5d7c664c37b98072c7569a09cdd20ac22b
DavisRayM <davisraymondmuro@outlook.com>
2018-06-25 10:39:24 +0300

    Add an additional optional prop of className for ElementMap

--- src/containers/ElementMap.js
+++ src/containers/ElementMap.js
@@ -1,21 +1,19 @@
 // This component takes a list of strings
 // and returns them  wrapped in the provided HTML tag
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class ElementMap extends Component {
-    render() {
-        const items = this.props.items;
-        // use React.createElement to create the html tag
-        // in a dynamic way
-        const listItems = items.map((item, index) =>
-            React.createElement(
-                this.props.HTMLTag,
-                {key: index},
-                [item]
-            )
-        );
-      return (
-        listItems
-      );
-    }
+  render() {
+    const items = this.props.items;
+    // use React.createElement to create the html tag
+    // in a dynamic way
+    const listItems = items.map((item, index) =>
+      React.createElement(
+        this.props.HTMLTag,
+        { key: index, className: this.props.className },
+        [item]
+      )
+    );
+    return listItems;
+  }
 }
