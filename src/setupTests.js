f6433f308cd67c97e7ce3bb39398a7c0d34e855f
DavisRayM <davisraymondmuro@outlook.com>
2018-06-25 16:53:46 +0300

    Modify setupTests

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,7 +4,8 @@ import Adapter from "enzyme-adapter-react-16";
 import fontawesome from "@fortawesome/fontawesome";
 import faSearch from "@fortawesome/fontawesome-free-solid/faSearch";
 import faCaretDown from "@fortawesome/fontawesome-free-solid/faCaretDown";
+import faClone from "@fortawesome/fontawesome-free-solid/faClone";
 
-fontawesome.library.add(faSearch, faCaretDown);
+fontawesome.library.add(faSearch, faCaretDown, faClone);
 
 configure({ adapter: new Adapter() });
