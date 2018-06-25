5ac4594f5b3e130f0031f1277f7f271d095dc1e3
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-06-25 19:20:12 +0300

    Add form reducer

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,14 +1,9 @@
 // Exports all reducers
-import global from './global/reducer';
-import tasks from './tasks/reducer';
-import users from './users/reducer'
-import locations from './locations/reducer'
-import clients from './clients/reducer'
+import global from "./global/reducer";
+import tasks from "./tasks/reducer";
+import users from "./users/reducer";
+import locations from "./locations/reducer";
+import clients from "./clients/reducer";
+import forms from "./forms/reducer";
 
-export {
-  tasks,
-  global,
-  users,
-  locations,
-  clients
-};
+export { tasks, global, users, locations, clients, forms };
