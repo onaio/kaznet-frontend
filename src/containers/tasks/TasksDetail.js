ae6881c08eebc5354626d9095276fd3021dc38c4
DavisRayM <davisraymondmuro@outlook.com>
2018-06-25 11:28:43 +0300

    Modify TaskDetail Component

--- src/containers/tasks/TasksDetail.js
+++ src/containers/tasks/TasksDetail.js
@@ -1,27 +1,43 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import "./TasksDetail.css";
 import _ from "lodash";
+import "./TasksDetail.css";
 
 import * as taskSelectors from "../../store/tasks/reducer";
 import * as taskActions from "../../store/tasks/actions";
 import * as globalActions from "../../store/global/actions";
 
-import DetailView from "../../components/TasksDetailView";
-import ElementMap from "../ElementMap";
+import DetailView from "../../components/DetailView";
+import NestedElementMap from "../NestedElementMap";
 
 export class TasksDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.title = "Undefined";
+  }
+
   componentDidMount() {
     this.props.fetchTask(this.props.match.params.id);
-    this.props.changePageTitle("Tasks >");
+    this.props.changePageTitle(`Tasks > Undefined`);
+  }
+
+  componentDidUpdate() {
+    this.props.changePageTitle(`Tasks > ${this.title}`);
   }
 
   render() {
-    if (!this.props.rowsById) return this.renderLoading();
+    const task = _.find(this.props.rowById, data => {
+      return data.id === this.props.match.params.id;
+    });
+    if (!task) return this.renderLoading();
+    this.title = task.attributes.name;
     return (
       <div className="TasksList">
-        <DetailView />
+        <DetailView
+          renderMainDetails={this.renderMainDetails(task)}
+          renderAdditionalDetails={this.renderAdditionalDetails(task)}
+        />
       </div>
     );
   }
@@ -29,11 +45,34 @@ export class TasksDetail extends Component {
   renderLoading() {
     return <p>Loading...</p>;
   }
+
+  renderMainDetails(data) {
+    const headerItems = {
+      Description: data.attributes.description,
+      Name: data.attributes.name,
+      "Unit Reward Amount": data.attributes.current_bounty_amount,
+      Form: data.attributes.xform_title
+    };
+
+    return <NestedElementMap detailitems={headerItems} HTMLTag="td" />;
+  }
+
+  renderAdditionalDetails(data) {
+    const headerItems = {
+      "Active dates": "Active Date Here",
+      Recurring: "Timing Rule Here",
+      Locations: "Location Here",
+      "Submission Limit": data.attributes.total_submission_target,
+      "Minimum Contributor Level": "Minimum Contributor Level here"
+    };
+
+    return <NestedElementMap detailitems={headerItems} HTMLTag="td" />;
+  }
 }
 
 function mapStateToProps(state) {
   return {
-    rowsById: taskSelectors.getTasksById(state)
+    rowById: taskSelectors.getTasksById(state)
   };
 }
 
