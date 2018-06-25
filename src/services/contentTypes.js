a8e703e8ec2a3762b1cbd680222a37f87c3b4996
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-06-25 19:55:40 +0300

    Add content type service

---
+++ src/services/contentTypes.js
@@ -0,0 +1,37 @@
+import _ from "lodash";
+import * as constants from "../constants";
+
+class contentTypeService {
+  async getContentTypeList() {
+    const url = `${constants.API_ENDPOINT}/contentTypes/?format=vnd.api%2Bjson`;
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/vnd.api+json",
+        Authorization: `Token ${constants.API_TOKEN}`
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error(
+        `ContentTypeService getContentTypeList failed, HTTP status ${
+          response.status
+        }`
+      );
+    }
+
+    const apiResponse = await response.json();
+    const data = _.get(apiResponse, "data");
+
+    if (!data) {
+      throw new Error(
+        `ContentTypeService getContentTypeList failed, data not returned`
+      );
+    }
+    return _.map(data, client => {
+      return client;
+    });
+  }
+}
+
+export default new contentTypeService();
