d0624079c4a417c01cad959221f65d6c98a5e149
DavisRayM <davisraymondmuro@outlook.com>
2018-06-25 11:27:06 +0300

    Add NestedElementMap Component

---
+++ src/containers/NestedElementMap.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import ElementMap from "./ElementMap";
+
+export default class NestedElementMap extends Component {
+  render() {
+    const detailitems = this.props.detailitems;
+    const details = [];
+    var counter = 1;
+
+    for (const [key, value] of Object.entries(detailitems)) {
+      details.push(
+        React.createElement("tr", { key: key }, [
+          <ElementMap
+            key={counter}
+            items={[key]}
+            HTMLTag="td"
+            className="kaznet-title-td"
+          />,
+          // Need the key to be different as such we are multiplying it by 20 from the first one.
+          <ElementMap key={counter * 20} items={[value]} HTMLTag="td" />
+        ])
+      );
+      counter++;
+    }
+
+    return details;
+  }
+}
