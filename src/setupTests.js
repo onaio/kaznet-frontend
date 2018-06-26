fb26b3b6deb65d3dad026b15902d530857c18646
DavisRayM <davisraymondmuro@outlook.com>
2018-06-26 16:35:09 +0300

    Update setupTests - Update Snapshots

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,8 +4,9 @@ import Adapter from "enzyme-adapter-react-16";
 import fontawesome from "@fortawesome/fontawesome";
 import faSearch from "@fortawesome/fontawesome-free-solid/faSearch";
 import faCaretDown from "@fortawesome/fontawesome-free-solid/faCaretDown";
+import faExternalLinkAlt from "@fortawesome/fontawesome-free-solid/faExternalLinkAlt";
 import faClone from "@fortawesome/fontawesome-free-solid/faClone";
 
-fontawesome.library.add(faSearch, faCaretDown, faClone);
+fontawesome.library.add(faSearch, faCaretDown, faClone, faExternalLinkAlt);
 
 configure({ adapter: new Adapter() });
