e58efa3105dca20fb2ddb2ccb9cc17db61b3e0e1
DavisRayM <davisraymondmuro@outlook.com>
2018-06-27 16:52:00 +0300

    Modify Select Component

--- src/containers/Select.js
+++ src/containers/Select.js
@@ -6,12 +6,26 @@ export class OptionMap extends Component {
   render() {
     const obj = this.props.obj;
 
-    var listItems = Object.entries(obj).map(item => (
-      <option key={item[0]} value={item[0]}>
-        {item[1].attributes[this.props.titleField]}
-      </option>
-    ));
+    if (obj != null) {
+      var listItems = Object.entries(obj).map(item => (
+        <option key={item[0]} value={item[0]}>
+          {item[1].attributes[this.props.titleField]}
+        </option>
+      ));
+    } else {
+      const list = this.props.list;
+      listItems = [];
 
+      for (var counter in list) {
+        listItems.push(
+          // We add one since counter starts at 0
+          // and clashes with line29s key
+          <option key={counter + 1} value={counter + 1}>
+            {list[counter].attributes[this.props.titleField]}
+          </option>
+        );
+      }
+    }
     listItems.unshift(<option key={0}>----</option>);
     return listItems;
   }
