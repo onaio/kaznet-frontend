8efc3e31bdb3ecf50d7bc17af05857a2381502f4
DavisRayM <davisraymondmuro@outlook.com>
2018-07-02 11:10:58 +0300

    Update TasksList test

--- src/containers/tasks/tests/TasksList.test.js
+++ src/containers/tasks/tests/TasksList.test.js
@@ -2,9 +2,13 @@
 import React from "react";
 import { shallow, mount } from "enzyme";
 import toJson from "enzyme-to-json";
+import { Router } from "react-router";
+import createBrowserHistory from "history/createBrowserHistory";
 
 import { TasksList } from "../TasksList";
 import * as fixtures from "../../../store/tasks/tests/fixtures";
+
+const history = createBrowserHistory();
 
 describe("containers/task/TasksList", () => {
   it("renders without crashing", () => {
@@ -21,15 +25,17 @@ describe("containers/task/TasksList", () => {
 
   it("renders task list correctly", () => {
     const wrapper = mount(
-      <TasksList
-        fetchTasks={function() {}}
-        changePageTitle={function() {}}
-        changePageTitleButton={function() {}}
-        changePageTarget={function() {}}
-        showListTitle={function() {}}
-        rowsById={fixtures.tasksById}
-        rowsIdArray={fixtures.tasksIdArray}
-      />
+      <Router history={history}>
+        <TasksList
+          fetchTasks={function() {}}
+          changePageTitle={function() {}}
+          changePageTitleButton={function() {}}
+          changePageTarget={function() {}}
+          showListTitle={function() {}}
+          rowsById={fixtures.tasksById}
+          rowsIdArray={fixtures.tasksIdArray}
+        />
+      </Router>
     );
     expect(toJson(wrapper)).toMatchSnapshot();
     wrapper.unmount();
