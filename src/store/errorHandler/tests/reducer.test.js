deae677ca80ba101218a0b3bd88cae2e30754dc8
DavisRayM <davisraymondmuro@outlook.com>
2018-07-02 14:03:49 +0300

    Add tests for errorHandler store

---
+++ src/store/errorHandler/tests/reducer.test.js
@@ -0,0 +1,38 @@
+// errorHandler reducer tests
+import _ from "lodash";
+import Immutable from "seamless-immutable";
+import { Reducer } from "redux-testkit";
+
+import errorHandler from "../reducer";
+import * as actionTypes from "../actionTypes";
+
+const initialState = Immutable({
+  errors: false,
+  errorMessage: null
+});
+
+describe("store/errorHandler/reducer", () => {
+  it("should have initial state", () => {
+    expect(errorHandler()).toEqual(initialState);
+  });
+
+  it("should not affect state", () => {
+    Reducer(errorHandler)
+      .expect({ type: "NOT_EXISTING" })
+      .toReturnState(initialState);
+  });
+
+  it("should change errors and errorMessage", () => {
+    const existingState = Immutable(initialState);
+    const newState = _.clone(initialState);
+    const errorMessage = "Error!!";
+    const action = { type: actionTypes.REQUEST_FAILURE, errorMessage };
+    newState.errors = true;
+    newState.errorMessage = errorMessage;
+
+    Reducer(errorHandler)
+      .withState(existingState)
+      .expect(action)
+      .toReturnState(newState);
+  });
+});
