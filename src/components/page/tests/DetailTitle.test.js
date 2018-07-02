0a8dd689c8cd6345f05be2bb6b896b910a8f1f46
DavisRayM <davisraymondmuro@outlook.com>
2018-07-02 12:39:14 +0300

    Add Tests for DetailTitle Component

---
+++ src/components/page/tests/DetailTitle.test.js
@@ -0,0 +1,38 @@
+// test DetailTitle
+import React from "react";
+import { shallow, mount } from "enzyme";
+import toJson from "enzyme-to-json";
+import { Router } from "react-router";
+import createBrowserHistory from "history/createBrowserHistory";
+
+import DetailTitle from "../DetailTitle";
+
+const history = createBrowserHistory();
+
+describe("components/page/DetailTitle", () => {
+  it("renders without crashing", () => {
+    shallow(
+      <DetailTitle
+        pageTitle={"Kaznet"}
+        detailName={"Testing!!!"}
+        pageTarget={"/sol"}
+        status={"Active"}
+      />
+    );
+  });
+
+  it("renders header correctly", () => {
+    const wrapper = mount(
+      <Router history={history}>
+        <DetailTitle
+          pageTitle={"Kaznet"}
+          detailName={"Testing!!!"}
+          pageTarget={"/sol"}
+          status={"Active"}
+        />
+      </Router>
+    );
+    expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper.unmount();
+  });
+});
