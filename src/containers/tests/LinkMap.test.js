5f2c09759f90d78fa7eb9e1ec6584f4e2c05e898
DavisRayM <davisraymondmuro@outlook.com>
2018-07-02 12:33:47 +0300

    Add Tests for LinkMap Component

---
+++ src/containers/tests/LinkMap.test.js
@@ -0,0 +1,35 @@
+// Test LinkMap
+import React from "react";
+import { shallow, mount } from "enzyme";
+import { Router } from "react-router";
+import LinkMap from "../LinkMap";
+import toJson from "enzyme-to-json";
+import createBrowserHistory from "history/createBrowserHistory";
+
+const history = createBrowserHistory();
+
+describe("containers/LinkMap", () => {
+  it("renders without crashing", () => {
+    const linkItems = {
+      test: "/",
+      amount: "/"
+    };
+    shallow(<LinkMap links={linkItems} />);
+  });
+
+  it("renders links correctly", () => {
+    const linkItems = {
+      test: "/",
+      amount: "/"
+    };
+
+    const wrapper = mount(
+      <Router history={history}>
+        <LinkMap links={linkItems} />
+      </Router>
+    );
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper.unmount;
+  });
+});
