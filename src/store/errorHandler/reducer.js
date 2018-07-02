61c65a01f30f83283c6bf6851220105ffd895db7
DavisRayM <davisraymondmuro@outlook.com>
2018-07-02 12:52:37 +0300

    Add errorHandler store

---
+++ src/store/errorHandler/reducer.js
@@ -0,0 +1,35 @@
+// ErrorHandler reducer
+import Immutable from "seamless-immutable";
+
+import * as types from "./actionTypes";
+
+const initialState = Immutable({
+  errors: false,
+  errorMessage: null
+});
+
+export default function reduce(state = initialState, action = {}) {
+  switch (action.type) {
+    case types.REQUEST_FAILURE:
+      return state.merge({
+        errors: true,
+        errorMessage: action.errorMessage
+      });
+    case types.REQUEST_SUCCESS:
+      return state.merge({
+        errors: false
+      });
+    default:
+      return state;
+  }
+}
+
+// Error Handler Selectors
+
+export function getIsRetrieving(state) {
+  return !state.errorHandler.errors;
+}
+
+export function getErrorMessage(state) {
+  return state.errorHandler.errorMessage;
+}
