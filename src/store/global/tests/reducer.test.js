21169feb6037b38fb4eaaca433d138d3967926b7
DavisRayM <davisraymondmuro@outlook.com>
2018-07-03 14:37:21 +0300

    Increase global store test coverage

--- src/store/global/tests/reducer.test.js
+++ src/store/global/tests/reducer.test.js
@@ -66,4 +66,74 @@ describe("store/global/reducer", () => {
       .expect(action)
       .toReturnState(newState);
   });
+
+  it("should toggle noTitle and showDetail and Set detailName", () => {
+    const existingState = Immutable(initialState);
+    const newState = _.clone(initialState);
+    const detailName = "Detail !!!";
+    const action = { type: actionTypes.TOGGLE_DETAIL_TITLE_ON, detailName };
+    newState.detailName = detailName;
+    newState.noTitle = false;
+    newState.showDetail = true;
+    Reducer(global)
+      .withState(existingState)
+      .expect(action)
+      .toReturnState(newState);
+  });
+
+  it("should toggle noTitle and showDetail", () => {
+    const existingState = Immutable(initialState);
+    const newState = _.clone(initialState);
+    const action = { type: actionTypes.TOGGLE_DETAIL_TITLE_OFF };
+    newState.noTitle = false;
+    newState.showDetail = false;
+    Reducer(global)
+      .withState(existingState)
+      .expect(action)
+      .toReturnState(newState);
+  });
+
+  it("should toggle noTitle", () => {
+    const existingState = Immutable(initialState);
+    const newState = _.clone(initialState);
+    const action = { type: actionTypes.PAGE_TITLE_OFF };
+    newState.noTitle = true;
+    Reducer(global)
+      .withState(existingState)
+      .expect(action)
+      .toReturnState(newState);
+
+    const existingState2 = Immutable(newState);
+    const newState2 = _.clone(existingState2);
+    newState2.noTitle = false;
+    const action2 = { type: actionTypes.PAGE_TITLE_ON };
+    Reducer(global)
+      .withState(existingState2)
+      .expect(action2)
+      .toReturnState(newState2);
+  });
+
+  it("should set action links", () => {
+    const existingState = Immutable(initialState);
+    const newState = _.clone(initialState);
+    const actionLinks = ["Hey", "Bye"];
+    const action = { type: actionTypes.SET_ACTION_LINKS, actionLinks };
+    newState.actionLinks = actionLinks;
+    Reducer(global)
+      .withState(existingState)
+      .expect(action)
+      .toReturnState(newState);
+  });
+
+  it("should change detail Status", () => {
+    const existingState = Immutable(initialState);
+    const newState = _.clone(initialState);
+    const detailStatus = "Active";
+    const action = { type: actionTypes.CHANGE_DETAIL_STATUS, detailStatus };
+    newState.detailStatus = detailStatus;
+    Reducer(global)
+      .withState(existingState)
+      .expect(action)
+      .toReturnState(newState);
+  });
 });
