df9a8ebc2b101fca08584709c745e81a84e89593
DavisRayM <davisraymondmuro@outlook.com>
2018-07-04 11:37:41 +0300

    Add TaskCreation Component

---
+++ src/containers/tasks/TaskCreation.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import moment from "moment";
+import TaskService from "../../services/tasks";
+import TaskForm from "./TaskForm";
+
+export default class TaskCreation extends Component {
+  render() {
+    const service = TaskService.createTask;
+    const initialData = {
+      name: "",
+      estimated_time: "15",
+      start: moment().format("YYYY-MM-DD"),
+      end: moment().format("YYYY-MM-DD"),
+      description: "",
+      required_expertise: "1",
+      timing_rule: "",
+      status: "d",
+      user_submission_target: 10,
+      amount: ""
+    };
+
+    return <TaskForm initialData={initialData} service={service} />;
+  }
+}
