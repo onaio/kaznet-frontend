1a9ab942bb1ae7f0cf96266c6a1417273444d389
DavisRayM <davisraymondmuro@outlook.com>
2018-07-04 13:57:02 +0300

    Add editTask service

--- src/services/tasks.js
+++ src/services/tasks.js
@@ -56,6 +56,39 @@ class TaskService {
     return data;
   }
 
+  async editTask(task_data, id) {
+    const url = `${constants.API_ENDPOINT}/tasks/${id}/`;
+    const response = await fetch(url, {
+      method: "PATCH",
+      headers: {
+        Accept: "application/vnd.api+json",
+        "content-type": "application/vnd.api+json",
+        Authorization: `Token ${constants.API_TOKEN}`
+      },
+      body: JSON.stringify(task_data),
+      cache: "no-cache"
+    });
+
+    if (!response.ok && response.status !== 400) {
+      throw new Error(
+        `TaskService editTask failed, HTTP status ${response.status}`
+      );
+    }
+
+    const apiResponse = await response.json();
+
+    if (response.status === 400) {
+      return apiResponse;
+    }
+
+    const data = _.get(apiResponse, "data");
+    if (!data) {
+      throw new Error("TaskService editTask failed, data not returned");
+    }
+
+    return data;
+  }
+
   async getTask(id) {
     const url = `${constants.API_ENDPOINT}/tasks/${id}/?format=vnd.api%2Bjson`;
     const response = await fetch(url, {
