8d88ffc7e94d319f0043b199a3803799d20ba0a3
DavisRayM <davisraymondmuro@outlook.com>
2018-07-05 12:39:14 +0300

    Add createClient to Clients Service

--- src/services/clients.js
+++ src/services/clients.js
@@ -28,6 +28,37 @@ class ClientService {
       return client;
     });
   }
+
+  async createClient(client_data) {
+    const url = `${constants.API_ENDPOINT}/clients/`;
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/vnd.api+json",
+        "content-type": "application/vnd.api+json",
+        Authorization: `Token ${constants.API_TOKEN}`
+      },
+      body: JSON.stringify(client_data),
+      cache: "no-cache"
+    });
+
+    if (!response.ok && response.status !== 400) {
+      throw new Error(
+        `ClientService createClient failed, HTTP status ${response.status}`
+      );
+    }
+
+    const apiResponse = await response.json();
+    if (response.status === 400) {
+      return apiResponse;
+    }
+    const data = _.get(apiResponse, "data");
+
+    if (!data) {
+      throw new Error(`ClientService createData failed, data not returned`);
+    }
+    return data;
+  }
 }
 
 export default new ClientService();
