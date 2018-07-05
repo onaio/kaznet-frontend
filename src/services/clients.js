f71387987cab5a98a2f140f2c12df99bf0c80158
DavisRayM <davisraymondmuro@outlook.com>
2018-07-05 18:31:51 +0300

    Add getClient to clients Services

--- src/services/clients.js
+++ src/services/clients.js
@@ -92,6 +92,37 @@ class ClientService {
 
     return data;
   }
+
+  async getClient(id) {
+    const url = `${
+      constants.API_ENDPOINT
+    }/clients/${id}/?format=vnd.api%2Bjson`;
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/vnd.api+json",
+        Authorization: `Token ${constants.API_TOKEN}`
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error(
+        `ClientService getClient failed, HTTP status ${response.status}`
+      );
+    }
+
+    const apiResponse = await response.json();
+
+    if (response.status === 400) {
+      throw apiResponse.errors;
+    }
+    const data = _.get(apiResponse, "data");
+
+    if (!data) {
+      throw new Error(`ClientService getClient failed, data not returned`);
+    }
+    return data;
+  }
 }
 
 export default new ClientService();
