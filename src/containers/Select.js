6e4a6cc578eeb239c863fef8d0a5d8b39e0208d6
DavisRayM <davisraymondmuro@outlook.com>
2018-07-05 11:15:43 +0300

    Update Select Component

--- src/containers/Select.js
+++ src/containers/Select.js
@@ -5,12 +5,22 @@ import React, { Component } from "react";
 export class OptionMap extends Component {
   render() {
     const obj = this.props.obj;
+    const additionalObj = this.props.additionalObj;
 
     var listItems = Object.entries(obj).map(item => (
       <option key={item[0]} value={item[0]}>
         {item[1].attributes[this.props.titleField]}
       </option>
     ));
+
+    // Adds a Single Object to the list of Items
+    if (additionalObj != null) {
+      listItems.push(
+        <option key={additionalObj.id} value={additionalObj.id}>
+          {additionalObj.attributes[this.props.titleField]}
+        </option>
+      );
+    }
 
     listItems.unshift(<option key={0}>----</option>);
     return listItems;
