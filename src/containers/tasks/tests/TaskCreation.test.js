60826e78b3c2f40db50d4338041e577e892fc515
DavisRayM <davisraymondmuro@outlook.com>
2018-07-05 11:50:24 +0300

    Update Tests for TaskEditForm Component and TaskCreation Component

--- src/containers/tasks/tests/TaskCreation.test.js
+++ src/containers/tasks/tests/TaskCreation.test.js
@@ -1,12 +1,35 @@
 // Test TaskCreation
 import React from "react";
 import { shallow, mount } from "enzyme";
+import moment from "moment";
 import toJson from "enzyme-to-json";
 
 import TaskCreation from "../TaskCreation";
+import FormView from "../../../components/FormView";
+import TaskForm from "../TaskForm";
 
 describe("containers/task/TaskCreation", () => {
   it("renders without crashing", () => {
     shallow(<TaskCreation />);
+  });
+
+  it("renders both Form View and TaskForm with correct Data", () => {
+    const initialData = {
+      name: "",
+      estimated_time: "15",
+      start: moment().format("YYYY-MM-DD"),
+      end: moment().format("YYYY-MM-DD"),
+      description: "",
+      required_expertise: "1",
+      timing_rule: "",
+      status: "d",
+      user_submission_target: 10,
+      amount: ""
+    };
+    const wrapper = shallow(<TaskCreation />).dive();
+    expect(wrapper.find(FormView)).toHaveLength(0);
+    expect(wrapper.find(TaskForm)).toHaveLength(1);
+    const data = wrapper.find(TaskForm).props().initialData;
+    expect(data).toEqual(initialData);
   });
 });
--- src/containers/tasks/tests/TaskEditForm.test.js
+++ src/containers/tasks/tests/TaskEditForm.test.js
@@ -3,7 +3,10 @@ import React from "react";
 import { shallow, mount } from "enzyme";
 import toJson from "enzyme-to-json";
 
+import * as fixtures from "../../../store/tasks/tests/fixtures";
 import { TaskEditForm } from "../TaskEditForm";
+import FormView from "../../../components/FormView";
+import TaskForm from "../TaskForm";
 
 describe("containers/task/TaskEditForm", () => {
   it("renders without crashing", () => {
@@ -18,5 +21,39 @@ describe("containers/task/TaskEditForm", () => {
         noTitle={function() {}}
       />
     );
+  });
+
+  it("renders both Form View and TaskForm with correct Data", () => {
+    const initialData = {
+      amount: 55,
+      client: "7",
+      descripton: undefined,
+      end: "2019-01-30",
+      estimated_time: 15,
+      form: 1,
+      name: "Awesome Task",
+      required_expertise: "1",
+      start: "2018-06-21",
+      status: "a",
+      timing_rule: "RRULE:FREQ=DAILY;INTERVAL=10;COUNT=12",
+      user_submission_target: 100
+    };
+    const wrapper = shallow(
+      <TaskEditForm
+        match={{
+          params: {
+            id: "1"
+          }
+        }}
+        fetchTask={function() {}}
+        noTitle={function() {}}
+        taskById={fixtures.taskById}
+      />
+    ).dive();
+
+    expect(wrapper.find(FormView)).toHaveLength(0);
+    expect(wrapper.find(TaskForm)).toHaveLength(1);
+    const data = wrapper.find(TaskForm).props().initialData;
+    expect(data).toEqual(initialData);
   });
 });
