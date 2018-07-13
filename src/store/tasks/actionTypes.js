a407cd621174094dac06d17ac27b00038f249f00
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-07-13 14:45:18 +0300

    Celanup

--- src/store/tasks/actionTypes.js
+++ src/store/tasks/actionTypes.js
@@ -1,6 +1,6 @@
 // tasks action types
 export const TASKS_FETCHED = "tasks.TASKS_FETCHED";
-export const TASK_CREATED = "tasks.TASKS_CREATED";
+export const TASK_CREATED = "tasks.TASK_CREATED";
 export const TASK_FETCHED = "tasks.TASK_FETCHED";
 export const TASK_EDITED = "tasks.TASK_EDITED";
 export const TASK_DELETED = "tasks.TASK_DELETED";
