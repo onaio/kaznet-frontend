3b8bf6cf31b1c2d5c5e3efbf4c5c29316b6fe3df
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-07-16 15:17:13 +0300

    Add LocationForm test

---
+++ src/containers/locations/tests/LocationForm.test.js
@@ -0,0 +1,57 @@
+// Test LocationForm
+import React from "react";
+import { shallow, mount } from "enzyme";
+import toJson from "enzyme-to-json";
+
+import ErrorBoundary from "../../../components/ErrorBoundary";
+import { LocationForm } from "../LocationForm";
+import * as fixtures from "../../../store/locations/tests/fixtures";
+import { locationTypesById } from "../../../store/locationTypes/tests/fixtures";
+
+describe("containers/client/LocationForm", () => {
+  it("renders without crashing", () => {
+    const initialData = {
+      name: "Kitui",
+      parent: "A county",
+      location_type: "",
+      geopoint: "36,-1",
+      radius: "9",
+      shapefile: ""
+    };
+    shallow(
+      <LocationForm
+        formActionDispatch={function() {}}
+        fetchLocations={function() {}}
+        fetchLocationTypes={function() {}}
+        initialData={initialData}
+        locationsById={fixtures.locationsById}
+        locationTypesById={locationTypesById}
+      />
+    );
+  });
+
+  it("renders client form correctly", () => {
+    const initialData = {
+      name: "Kitui",
+      parent: "A county",
+      location_type: "",
+      geopoint: "36,-1",
+      radius: "9",
+      shapefile: ""
+    };
+    const wrapper = mount(
+      <ErrorBoundary>
+        <LocationForm
+          formActionDispatch={function() {}}
+          fetchLocations={function() {}}
+          fetchLocationTypes={function() {}}
+          initialData={initialData}
+          locationsById={fixtures.locationsById}
+          locationTypesById={locationTypesById}
+        />
+      </ErrorBoundary>
+    );
+    expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper.unmount();
+  });
+});
