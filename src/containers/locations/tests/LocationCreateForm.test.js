0968939a3cf8d49f1cb2f274d9c4972e8d1490a7
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-07-16 15:32:06 +0300

    Add LocationCreateForm test

---
+++ src/containers/locations/tests/LocationCreateForm.test.js
@@ -0,0 +1,32 @@
+// Test LocationCreation
+import React from "react";
+import { shallow } from "enzyme";
+import moment from "moment";
+
+import { LocationCreateForm } from "../LocationCreateForm";
+import FormView from "../../../components/FormView";
+import LocationForm from "../LocationForm";
+
+describe("containers/location/LocationCreateForm", () => {
+  it("renders without crashing", () => {
+    shallow(<LocationCreateForm noTitle={function() {}} />);
+  });
+
+  it("renders both Form View and LocationForm with correct Data", () => {
+    const initialData = {
+      name: "",
+      parent: "",
+      location_type: "",
+      geopoint: "",
+      radius: "",
+      shapefile: ""
+    };
+    const wrapper = shallow(
+      <LocationCreateForm noTitle={function() {}} />
+    ).dive();
+    expect(wrapper.find(FormView)).toHaveLength(0);
+    expect(wrapper.find(LocationForm)).toHaveLength(1);
+    const data = wrapper.find(LocationForm).props().initialData;
+    expect(data).toEqual(initialData);
+  });
+});
