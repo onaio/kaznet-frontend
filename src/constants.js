41d4cbe94090ce807c70a6af47a2536ae8fea8d2
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-07-18 18:27:54 +0300

    Add ona username and site constants

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,7 @@
 export const API_ENDPOINT = process.env.REACT_APP_KAZNET_ENDPOINT;
 export const API_TOKEN = process.env.REACT_APP_API_TOKEN;
+export const ONA_WEBSITE = process.env.REACT_APP_ONA_WEBSITE;
+export const ONA_USERNAME = process.env.REACT_APP_ONA_USERNAME;
 export const TASK_ACTIVE = "a";
 export const TASK_DEACTIVATED = "b";
 export const TASK_EXPIRED = "c";
