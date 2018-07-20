9a11f0575e08aad51d7eb090716e7afd4adcbd54
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-07-20 16:51:37 +0300

    Add location type thunks
    - Add action types
    - Add actions
    	- createLocationType
    	- editLocationType
    	- fetchLocationType
    	- deleteLocationType
    - Add tests

--- src/store/locationTypes/actionTypes.js
+++ src/store/locationTypes/actionTypes.js
@@ -1,2 +1,6 @@
 // locationTypes action types
 export const LOCATIONTYPES_FETCHED = "locationTypes.LOCATIONTYPES_FETCHED";
+export const LOCATIONTYPE_CREATED = "locations.LOCATIONTYPE_CREATED";
+export const LOCATIONTYPE_FETCHED = "locations.LOCATIONTYPE_FETCHED";
+export const LOCATIONTYPE_EDITED = "locations.LOCATIONTYPE_EDITED";
+export const LOCATIONTYPE_DELETED = "locations.LOCATIONTYPE_DELETED";
