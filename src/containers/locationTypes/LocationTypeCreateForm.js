a7df07a21ef43b98c8f0b219adae26e20df47402
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-07-20 17:18:55 +0300

    Add LocationTypeCreateForm

---
+++ src/containers/locationTypes/LocationTypeCreateForm.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
+import * as locationTypeActions from "../../store/locationTypes/actions";
+import LocationTypeForm from "./LocationTypeForm";
+import FormView from "../../components/FormView";
+import * as globalActions from "../../store/global/actions";
+
+export class LocationTypeCreateForm extends Component {
+  componentDidMount() {
+    this.props.noTitle();
+  }
+
+  render() {
+    const action = locationTypeActions.createLocationType;
+    const initialData = {
+      name: ""
+    };
+    return (
+      <FormView
+        form={<LocationTypeForm action={action} initialData={initialData} />}
+      />
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      noTitle: globalActions.toggleTitleOff
+    },
+    dispatch
+  );
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(LocationTypeCreateForm);
---
+++ src/containers/locationTypes/tests/LocationTypeCreateForm.test.js
@@ -0,0 +1,21 @@
+// Test LocationTypeCreateForm
+import React from "react";
+import { shallow } from "enzyme";
+
+import { LocationTypeCreateForm } from "../LocationTypeCreateForm";
+import FormView from "../../../components/FormView";
+import LocationTypeForm from "../LocationTypeForm";
+
+describe("containers/locationType/LocationTypeCreateForm", () => {
+  it("renders without crashing", () => {
+    shallow(<LocationTypeCreateForm noTitle={function() {}} />);
+  });
+
+  it("renders both Form View and LocationTypeForm", () => {
+    const wrapper = shallow(
+      <LocationTypeCreateForm noTitle={function() {}} />
+    ).dive();
+    expect(wrapper.find(FormView)).toHaveLength(0);
+    expect(wrapper.find(LocationTypeForm)).toHaveLength(1);
+  });
+});
