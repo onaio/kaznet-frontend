62cc2c98c4615ac01480dd8c375a58dcffd19555
Njagi Mwaniki <njagi@urbanslug.com>
2018-07-20 17:32:48 +0300

    Add pagination to user list container
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- src/containers/users/tests/UsersList.test.js
+++ src/containers/users/tests/UsersList.test.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { shallow, mount } from "enzyme";
 import toJson from "enzyme-to-json";
+import { Router } from "react-router";
+import createBrowserHistory from "history/createBrowserHistory";
 
 import { UsersList } from "../UsersList";
 import * as fixtures from "../../../store/users/tests/fixtures";
+
+const history = createBrowserHistory();
 
 describe("containers/users/UsersList", () => {
   it("renders without crashing", () => {
@@ -19,14 +23,22 @@ describe("containers/users/UsersList", () => {
 
   it("renders user list correctly", () => {
     const wrapper = mount(
-      <UsersList
-        fetchUsers={function() {}}
-        changePageTitle={function() {}}
-        changePageTitleButton={function() {}}
-        showListTitle={function() {}}
-        rowsById={fixtures.usersById}
-        rowsIdArray={fixtures.usersIdArray}
-      />
+      <Router history={history}>
+        <UsersList
+          fetchUsers={function() {}}
+          changePageTitle={function() {}}
+          changePageTitleButton={function() {}}
+          showListTitle={function() {}}
+          rowsById={fixtures.usersById}
+          rowsIdArray={fixtures.usersIdArray}
+          endpoint={"userprofiles"}
+          pageLinks={fixtures.pageLinks}
+          totalPages={fixtures.totalPages}
+          currentPage={fixtures.currentPage}
+          firstPage={fixtures.firstPage}
+          lastPage={fixtures.lastPage}
+        />
+      </Router>
     );
     expect(toJson(wrapper)).toMatchSnapshot();
     wrapper.unmount();
