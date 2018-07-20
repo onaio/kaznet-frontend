26f8726a13532881b2decba4ae9edc51fe30f8dc
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-07-20 17:14:37 +0300

    Add LocationTypeForm

---
+++ src/containers/locationTypes/tests/LocationTypeForm.test.js
@@ -0,0 +1,30 @@
+// Test LocationTypeForm
+import React from "react";
+import { shallow, mount } from "enzyme";
+import toJson from "enzyme-to-json";
+
+import { LocationTypeForm } from "../LocationTypeForm";
+
+describe("containers/locationType/LocationTypeForm", () => {
+  it("renders without crashing", () => {
+    const initialData = { name: "Market" };
+    shallow(
+      <LocationTypeForm
+        formActionDispatch={function() {}}
+        initialData={initialData}
+      />
+    );
+  });
+
+  it("renders locationType form correctly", () => {
+    const initialData = { name: "Market" };
+    const wrapper = mount(
+      <LocationTypeForm
+        formActionDispatch={function() {}}
+        initialData={initialData}
+      />
+    );
+    expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper.unmount();
+  });
+});
