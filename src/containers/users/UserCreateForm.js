ae50b5a056b2e61501036c568038ae235a178dbb
DavisRayM <davisraymondmuro@outlook.com>
2018-08-14 17:22:15 +0300

    Add UserForm Container - Add UserCreateForm

---
+++ src/containers/users/UserCreateForm.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
+import * as userActions from "../../store/users/actions";
+import UserForm from "./UserForm";
+import FormView from "../../components/FormView";
+import * as globalActions from "../../store/global/actions";
+
+export class UserCreateForm extends Component {
+  componentDidMount() {
+    this.props.noTitle();
+  }
+
+  render() {
+    const action = userActions.fetchUsers;
+    const initialData = {
+      first_name: "",
+      last_name: "",
+      email: "",
+      password: "",
+      gender: "",
+      role: "",
+      expertise: "",
+      national_id: "",
+      payment_number: "",
+      phone_number: "",
+      ona_username: ""
+    };
+
+    return (
+      <FormView
+        form={
+          <UserForm
+            initialData={initialData}
+            action={action}
+            redirectAfterAction="/users"
+          />
+        }
+      />
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      noTitle: globalActions.toggleTitleOff
+    },
+    dispatch
+  );
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(UserCreateForm);
