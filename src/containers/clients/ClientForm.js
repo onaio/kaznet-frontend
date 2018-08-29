99ee40e2fd69b2ab29a3d1148fad57113f091b82
Lincoln Simba <lincolncmba@gmail.com>
2018-08-29 16:43:13 +0300

    Provide extra padding  on form

--- src/containers/clients/ClientForm.js
+++ src/containers/clients/ClientForm.js
@@ -69,7 +69,7 @@ export class ClientForm extends Component {
           isSubmitting,
           setStatus
         }) => (
-          <div>
+          <div className="my-5">
             <Form onSubmit={handleSubmit}>
               <FormGroup className="row">
                 <Col sm="12">
@@ -89,7 +89,7 @@ export class ClientForm extends Component {
                   )}
                 </Col>
               </FormGroup>
-              <FormGroup className="row">
+              <FormGroup className="row my-5">
                 <Col md={{ size: 5, offset: 1 }}>
                   <Button
                     className="btn btn-secondary btn-block"
