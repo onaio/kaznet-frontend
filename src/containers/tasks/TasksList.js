d1329b02fcb9d73cceed617a1e3c96fd252b5bdd
Kahama <Kahama94@gmail.com>
2018-08-30 09:00:58 +0300

    Add matching search for task and client listviews

--- src/containers/tasks/TasksList.js
+++ src/containers/tasks/TasksList.js
@@ -22,7 +22,7 @@ export class TasksList extends Component {
   async componentDidMount() {
     this.props.showListTitle();
     this.props.changePageTitle("Tasks");
-    this.props.changePageTitleButton("+ Add Tasks");
+    this.props.changePageTitleButton("+ Create Task");
     this.props.changePageTarget("/tasks/new");
 
     let { search } = queryString.parse(this.props.location.search);
@@ -32,7 +32,6 @@ export class TasksList extends Component {
       search = "";
     }
     this.props.searchVal(search);
-
     let pageNumber = Number(page);
 
     if (isNaN(pageNumber)) {
@@ -66,6 +65,8 @@ export class TasksList extends Component {
         `${constants.API_ENDPOINT}/tasks/?search=${search}&page=${pageNumber}`
       );
       this.props.changePageNumber(pageNumber);
+    } else {
+      this.props.fetchTasks();
     }
   }
 
