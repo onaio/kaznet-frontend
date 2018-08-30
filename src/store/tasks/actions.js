fdd84063bc8aeee99270fd8c19836ae186089dfa
Kahama <Kahama94@gmail.com>
2018-08-30 09:00:58 +0300

    Add matching search for task and client listviews

--- src/store/tasks/actions.js
+++ src/store/tasks/actions.js
@@ -31,14 +31,7 @@ export function fetchTasks(url) {
   };
 }
 //get search value on task view
-export function getSearchVal(val) {
-  return async (dispatch, getState) => {
-    dispatch({
-      type: types.TASK_SEARCH_VAL,
-      val
-    });
-  };
-}
+
 // Change the current Page on the task list
 export function changePageNumber(pageNumber) {
   return async (dispatch, getState) => {
