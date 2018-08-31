5f96c21a8ca2947e0b459658b6578298de9f2f5e
Lincoln Simba <lincolncmba@gmail.com>
2018-08-31 14:18:22 +0300

    Tests for Location deletion. Fixes #121

---
+++ src/components/location/tests/LocationDetailTitle.test.js
@@ -0,0 +1,35 @@
+// Test LocationDetailTitle
+import React from "react";
+import { shallow, mount } from "enzyme";
+import toJson from "enzyme-to-json";
+import { Router } from "react-router";
+import createBrowserHistory from "history/createBrowserHistory";
+
+import ErrorBoundary from "../../ErrorBoundary";
+import { singleLocation } from "../../../store/locations/tests/fixtures";
+import LocationDetailTitle from "../LocationDetailTitle";
+
+const history = createBrowserHistory();
+
+describe("components/location/LocationDetailTitle", () => {
+  it("renders without crashing", () => {
+    shallow(
+      <ErrorBoundary>
+        <LocationDetailTitle location={singleLocation} />
+      </ErrorBoundary>
+    );
+  });
+  it("renders the correct html", () => {
+    const wrapper = mount(
+      <ErrorBoundary>
+        <Router history={history}>
+          <LocationDetailTitle location={singleLocation} />
+        </Router>
+      </ErrorBoundary>
+    );
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+
+    wrapper.unmount();
+  });
+});
---
+++ src/containers/locations/tests/LocationDeletion.test.js
@@ -0,0 +1,43 @@
+// test LocationDeletion
+import React from "react";
+import { shallow, mount } from "enzyme";
+import { Router } from "react-router";
+import createBrowserHistory from "history/createBrowserHistory";
+
+import { LocationDeletion } from "../LocationDeletion";
+
+const history = createBrowserHistory();
+
+describe("containers/locations/LocationDeletion", () => {
+  it("renders without crashing", () => {
+    shallow(
+      <LocationDeletion
+        match={{
+          params: {
+            id: "4"
+          }
+        }}
+        deleteLocation={function() {}}
+      />
+    );
+  });
+  it("check if deleteLocation works", () => {
+    const mockDeleteLocation = jest.fn();
+
+    const wrapper = mount(
+      <Router history={history}>
+        <LocationDeletion
+          match={{
+            params: {
+              id: "4"
+            }
+          }}
+          fetchLocation={function() {}}
+          deleteLocation={mockDeleteLocation}
+        />
+      </Router>
+    );
+    expect(mockDeleteLocation.mock.calls[0][0]).toEqual("4");
+    expect(mockDeleteLocation.mock.calls.length).toBe(1);
+  });
+});
