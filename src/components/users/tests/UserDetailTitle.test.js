fea9e6b6e9723325694fc1c9f7593f8177e0609b
Lincoln Simba <lincolncmba@gmail.com>
2018-09-05 09:27:41 +0300

    Tests: - User detail page - User deletion

---
+++ src/components/users/tests/UserDetailTitle.test.js
@@ -0,0 +1,35 @@
+// Test UserDetailTitle
+import React from "react";
+import { shallow, mount } from "enzyme";
+import toJson from "enzyme-to-json";
+import { Router } from "react-router";
+import createBrowserHistory from "history/createBrowserHistory";
+
+import ErrorBoundary from "../../ErrorBoundary";
+import { singleUser } from "../../../store/users/tests/fixtures";
+import UserDetailTitle from "../UserDetailTitle";
+
+const history = createBrowserHistory();
+
+describe("components/users/UserDetailTitle", () => {
+  it("renders without crashing", () => {
+    shallow(
+      <ErrorBoundary>
+        <UserDetailTitle user={singleUser} />
+      </ErrorBoundary>
+    );
+  });
+  it("renders the correct html", () => {
+    const wrapper = mount(
+      <ErrorBoundary>
+        <Router history={history}>
+          <UserDetailTitle user={singleUser} />
+        </Router>
+      </ErrorBoundary>
+    );
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+
+    wrapper.unmount();
+  });
+});
