690fe075d9151e620c3509ea132d3c96c920953d
Lincoln Simba <lincolncmba@gmail.com>
2018-09-07 11:14:57 +0300

    Remove created by paragraph in user details page

--- src/components/users/UserDetailTitle.js
+++ src/components/users/UserDetailTitle.js
@@ -2,7 +2,6 @@
 import React, { Component } from "react";
 import { Row, Col, Modal, ModalHeader, ModalFooter, Button } from "reactstrap";
 import { Link } from "react-router-dom";
-import Moment from "react-moment";
 
 import "../page/DetailTitle.css";
 import { ADMIN_ROLE } from "../../constants";
@@ -34,19 +33,9 @@ export default class UserDetailTitle extends Component {
               </Link>{" "}
               > {this.props.user.attributes.ona_username}
             </h1>
-            <p className="kaznet-creation-detail">
-              {"By "}
-              {this.props.user.attributes.created_by_name},{" "}
-              <Moment format="DD-MM-YYYY">
-                {this.props.user.attributes.created}
-              </Moment>
-            </p>
             <Col md="12">
               <Row className="kaznet-action-links">
-                <Link
-                  to={`/users/edit/${this.props.user.attributes.id}`}
-                  className="action-link"
-                >
+                <Link to={`#`} className="action-link">
                   EDIT
                 </Link>
                 {(this.props.user.attributes.submission_count > 0 ||
