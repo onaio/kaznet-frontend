06d3857d86e26654af7ce9c2f7cfe3692f3e2198
Lincoln Simba <lincolncmba@gmail.com>
2018-09-07 11:57:36 +0300

    Remove Edit and Delete button from user profile detail page

--- src/components/users/UserDetailTitle.js
+++ src/components/users/UserDetailTitle.js
@@ -1,10 +1,9 @@
 // Renders the detail page title section
 import React, { Component } from "react";
-import { Row, Col, Modal, ModalHeader, ModalFooter, Button } from "reactstrap";
+import { Col } from "reactstrap";
 import { Link } from "react-router-dom";
 
 import "../page/DetailTitle.css";
-import { ADMIN_ROLE } from "../../constants";
 
 export default class UserDetailTitle extends Component {
   constructor(props) {
@@ -32,45 +31,7 @@ export default class UserDetailTitle extends Component {
                 Users
               </Link>{" "}
               > {this.props.user.attributes.ona_username}
-            </h1>
-            <Col md="12">
-              <Row className="kaznet-action-links">
-                <Link to={`#`} className="action-link">
-                  EDIT
-                </Link>
-                {(this.props.user.attributes.submission_count > 0 ||
-                  this.props.user.attributes.role !== ADMIN_ROLE) && (
-                  <Button
-                    color="link"
-                    className="remove_button_css action-link action-link-alert"
-                    onClick={this.toggle}
-                  >
-                    &nbsp;&nbsp;|&nbsp;&nbsp; DELETE USER
-                  </Button>
-                )}
-                <Modal
-                  isOpen={this.state.modal}
-                  toggle={this.toggle}
-                  className={this.props.className}
-                >
-                  <ModalHeader toggle={this.toggle}>
-                    Are you sure you want to delete this USER?
-                  </ModalHeader>
-                  <ModalFooter>
-                    <Link
-                      to={`/users/${this.props.user.id}/delete`}
-                      className="btn btn-danger"
-                      onClick={this.toggle}
-                    >
-                      Delete User
-                    </Link>
-                    <Button color="secondary" onClick={this.toggle}>
-                      Cancel
-                    </Button>
-                  </ModalFooter>
-                </Modal>
-              </Row>
-            </Col>
+            </h1>&nbsp;&nbsp;
           </div>
         </Col>
       </section>
