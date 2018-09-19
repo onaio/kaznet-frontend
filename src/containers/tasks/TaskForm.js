03b8c80d20a64a6e899b9894f5e1b2fbf462c8e0
Kahama <Kahama94@gmail.com>
2018-09-19 18:07:51 +0300

    add snapshot and  TaskForm fixes

--- src/containers/tasks/TaskForm.js
+++ src/containers/tasks/TaskForm.js
@@ -14,7 +14,7 @@ import {
   FormText
 } from "reactstrap";
 import moment from "moment";
-import "react-rrule-generator/build/styles.css";
+
 import RRuleGenerator from "react-rrule-generator";
 import { Redirect } from "react-router-dom";
 import FontAwesomeIcon from "@fortawesome/react-fontawesome";
