61fc841442f28e5f32f8e74b988403aa1315e6ac
Lincoln Simba <lincolncmba@gmail.com>
2018-09-20 15:44:56 +0300

    Upload shapefiles. Upload zipped file

--- src/containers/locations/LocationForm.js
+++ src/containers/locations/LocationForm.js
@@ -38,11 +38,34 @@ const transformMyApiErrors = function(array) {
 export class LocationForm extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      shapefile: null
+    };
     this.targetId = props.targetId || null;
     this.getLocationTypeOptions.bind(this);
     this.loadLocationTypeOptions.bind(this);
     this.getParentOptions.bind(this);
     this.loadParentOptions.bind(this);
+    this.getOptions.bind(this);
+    this.loadOptions.bind(this);
+    this.onChangeShapefile = this.onChangeShapefile.bind(this);
+  }
+
+  onChangeShapefile(e) {
+    let files = e.target.files || e.dataTransfer.files;
+    if (files.length) {
+      this.createShapefile(files[0]);
+    }
+  }
+
+  createShapefile(file) {
+    let reader = new FileReader();
+    reader.onload = e => {
+      this.setState({
+        shapefile: new Uint8Array(e.target.result)
+      });
+    };
+    reader.readAsArrayBuffer(file);
   }
 
   componentDidMount() {
@@ -94,9 +117,17 @@ export class LocationForm extends Component {
                 location_type: values.location_type
                   ? { type: "LocationType", id: values.location_type.value }
                   : undefined,
-                geopoint: _.toString(values.geopoint),
-                radius: values.radius,
-                shapefile: values.shapefile ? values.shapefile : undefined
+                geopoint:
+                  !this.state.shapefile && values.geopoint
+                    ? _.toString(values.geopoint)
+                    : undefined,
+                radius:
+                  !this.state.shapefile && values.radius
+                    ? values.radius
+                    : undefined,
+                shapefile: this.state.shapefile
+                  ? this.state.shapefile
+                  : undefined
               }
             }
           };
@@ -302,6 +333,30 @@ export class LocationForm extends Component {
                   </FormText>
                   {errors.radius && (
                     <div className="invalid-feedback">{errors.radius}</div>
+                  )}
+                </Col>
+              </FormGroup>
+              <FormGroup className="row">
+                <Col sm="3">
+                  <Label for="shapefile">Shapefile</Label>
+                </Col>
+                <Col md="8">
+                  <Input
+                    name="shapefile"
+                    type="file"
+                    bsSize="lg"
+                    placeholder="Shapefile"
+                    aria-label="Shapefile"
+                    onChange={this.onChangeShapefile}
+                    value={values.shapefile}
+                    accept=".zip,application/octet-stream,application/zip,application/x-zip,application/x-zip-compressed"
+                    className={errors.shapefile ? "is-invalid" : ""}
+                  />
+                  <FormText color="muted">
+                    The zipped file of the shapefile
+                  </FormText>
+                  {errors.shapefile && (
+                    <div className="invalid-feedback">{errors.shapefile}</div>
                   )}
                 </Col>
               </FormGroup>
