e15c3ba265b47227eb7ece3a0d150b741cf53b4a
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-02-08 16:53:50 +0300

    Add tests for `MisconfiguredForm`

---
+++ src/components/forms/tests/MisconfiguredForm.test.js
@@ -0,0 +1,23 @@
+// test ExportModal
+import React from "react";
+import { shallow, mount } from "enzyme";
+import toJson from "enzyme-to-json";
+
+import MisconfiguredForm from "../MisconfiguredForm";
+import { formIdThreeById } from "../../../store/forms/tests/fixtures";
+
+describe("components/forms/MisconfiguredForm", () => {
+  it("renders without crashing", () => {
+    shallow(<MisconfiguredForm form={formIdThreeById} />);
+  });
+  it("renders the MisconfiguredForm component", () => {
+    const div = document.createElement("div");
+    document.body.appendChild(div);
+    const wrapper = mount(<MisconfiguredForm form={formIdThreeById} />, {
+      attachTo: div
+    });
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper.unmount();
+  });
+});
