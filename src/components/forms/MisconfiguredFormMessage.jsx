f3caeeada82e9e06b187e5e48a27747a4aaafcfc
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-02-20 19:55:10 +0300

    Refactor MisconfiguredFormMessage
    - Rewrite as pure function
    - Change to jsx
    - Fix lint errors
    - Fix broken `href`

---
+++ src/components/forms/MisconfiguredFormMessage.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { ONA_WEBSITE, WEBSITE_NAME } from '../../constants';
+
+function MisconfiguredFormMessage() {
+  return (
+    <div>
+      <p>
+        This form may not appear correctly in the
+        {WEBSITE_NAME}
+        mobile app. Additionally, it
+        <strong>may not be possible to submit data</strong>
+        to this form. Please
+        <a href={`${ONA_WEBSITE}`} target="_blank" rel="noopener noreferrer">
+          click this link to fix this form
+        </a>
+        . You can fix it be ensuring that:
+      </p>
+      <ol>
+        <li>The form belongs to the right organisation</li>
+        <li>The form exists in a project which allows its members to make form submissions</li>
+      </ol>
+    </div>
+  );
+}
+
+export default MisconfiguredFormMessage;
